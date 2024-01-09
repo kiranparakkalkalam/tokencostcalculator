8678f4a33d7d46b9143c386e3e4eb980f8961a89
Kiran Parakkalkalam <pkiran.kala@gmail.com>
2024-01-09 11:05:38 +0530

    Made things to show up in a table

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from tokencost import calculate_prompt_cost, USD_PER_TPU
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*", "allow_headers": ["Content-Type", "Authorization"]}})
 app.config['DEBUG'] = True
-app.config['CORS_SUPPORTS_CREDENTIALS'] = True
+app.config['CORS_SUPPORTS_CREDENTIALS'] = False
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.route('/api/token-cost-utility', methods=['POST'])
 def calculate_token_cost():
@@ -15,14 +19,13 @@ def calculate_token_cost():
     prompt_cost = calculate_prompt_cost(prompt=prompt, model=model)
     prompt_cost_in_usd = float(prompt_cost/USD_PER_TPU)
     prompt_decimal_notation = ("%.17f" % prompt_cost_in_usd).rstrip('0').rstrip('.')
-    response_data = {
+    response_data = {"response": {
         'model': model,
         'prompt_cost': prompt_cost,
         'prompt_cost_in_usd': prompt_decimal_notation
-    }
+    }}
     response = jsonify(response_data)
-    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
